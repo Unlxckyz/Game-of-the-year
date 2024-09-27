151c6b17cc904fd31d13919c421c6dbfcd78e0a3
Vitor Gouvea <112331228+milimiliel@users.noreply.github.com>
2024-09-27 13:04:21 -0300

    Enemy class reorder

--- classes/enemy.gd
+++ classes/enemy.gd
@@ -4,20 +4,20 @@ class_name Enemy
 @export var health : float
 @export var health_bar : HealthBar
 @export var atk : float
+@export var damage_modifiers : Dictionary = {}
 @export var def : float
-@export var hitbox : Area2D
-@export var spells : Array[Spell]
-@export var speed : float
-@export var AI_behavior : Node
 @export var level : int
+@export var speed : float
+@export var resistances : Dictionary = {}
+@export var spells : Array[Spell] = []
+@export var aggro_range : float
+@export var hitbox : Area2D
+@export var AI_behavior : Node
 @export var experience_points : int
 @export var drop_items : Array[Item] = []
 @export var is_boss : bool
-@export var aggro_range : float
-@export var resistances : Dictionary = {}
-@export var damage_modifiers : Dictionary = {}
+@export var is_elite: bool
 @export var animation_player : AnimationPlayer
-
 
 func _ready() -> void:
 	pass
